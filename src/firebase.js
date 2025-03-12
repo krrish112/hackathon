8536f9d1e1646f9e2df2a582eb90c90129a4e7b3
krrish112 <69080744+krrish112@users.noreply.github.com>
2025-03-12 21:44:16 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abc123def456"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
